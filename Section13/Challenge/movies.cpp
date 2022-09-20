4b94e3c99b4c34d21cb0c4c50a86814b03a9e63b
Giammarco <giammarco.boscaro@mailbox.org>
2022-09-20 21:56:08 +0200

    improved code

--- Section13/Challenge/movies.cpp
+++ Section13/Challenge/movies.cpp
@@ -13,14 +13,17 @@ Movies::~Movies() {
 }
 
 void Movies::display() const {
-    for(Movie m: this->collection) {
+    if (this->collection.size() == 0) {
+        cout << "No movies" << endl;
+    }
+    for(const Movie &m: this->collection) {
         m.display();
         cout << endl;
     }
 }
 
 void Movies::add(const Movie &movie) {
-    for(Movie &m: this->collection) {
+    for(const Movie &m: this->collection) {
         if(m.get_name() == movie.get_name()) {
             return;
         }
@@ -29,7 +32,7 @@ void Movies::add(const Movie &movie) {
 }
 
 void Movies::add(const Movie &&movie) {
-    for(Movie &m: this->collection) {
+    for(const Movie &m: this->collection) {
         if(m.get_name() == movie.get_name()) {
             return;
         }
@@ -41,7 +44,7 @@ void Movies::watch(const string name) {
     for(Movie &m: this->collection) {
         if(m.get_name() == name) {
             m.watch();
-            break;
+            return;
         }
     }
 }
